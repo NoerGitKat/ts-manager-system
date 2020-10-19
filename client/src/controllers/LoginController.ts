9910926fd52b8616cb178fd443b37c2d594390b6
Noer Paanakker <noer@lightworksweb.nl>
2020-10-19 10:42:04 +0200

    added validation

--- client/src/controllers/LoginController.ts
+++ client/src/controllers/LoginController.ts
@@ -1,13 +1,19 @@
 import BaseController from "./BaseController";
 
-
-
 class LoginController extends BaseController {
   public createView(): HTMLDivElement {
     const title = this.createDomEl("h2", "Please Login Here!");
     const username = this.createDomEl("label", "Username:");
     const password = this.createDomEl("label", "Password:");
-    const loginButton = this.createDomEl("button", "Login!");
+    const loginButton = this.createDomEl("button", "Login!", () => {
+      if (usernameInput.value && passwordInput.value) {
+        error.style.visibility = "hidden";
+      } else {
+        error.innerText = "Please fill in all fields.";
+        error.style.color = "red";
+        error.style.visibility = "visible";
+      }
+    });
     const breakEl = this.createDomEl("br", null);
 
     const usernameInput = this.createInputEl({
@@ -20,6 +26,9 @@ class LoginController extends BaseController {
       placeholder: "Password",
     });
 
+    // Validation feedback
+    const error = this.createDomEl("label");
+
     this.container.append(
       title,
       username,
@@ -27,13 +36,12 @@ class LoginController extends BaseController {
       password,
       passwordInput,
       breakEl,
-      loginButton
+      loginButton,
+      error
     );
 
     return this.container;
   }
-
- 
 }
 
 export default LoginController;
